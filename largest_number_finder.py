8ebbc730f67f97c272510ff9ca5d912b44a65a82
Ram Naik <g>
2023-08-31 17:46:58 +0530

    Made changes in the text

--- largest_number_finder.py
+++ largest_number_finder.py
@@ -1,16 +1,19 @@
 import streamlit as st
 
-def largest_number(num1, num2, num3):
-    return max(num1, num2, num3)
-
 st.title("Largest Number Finder")
+
+st.markdown("""
+Welcome to the Largest Number Finder! 🚀
+
+Enter three numbers, and we'll help you discover the largest among them.
+""")
 
 num1 = st.number_input("Enter the first number:")
 num2 = st.number_input("Enter the second number:")
 num3 = st.number_input("Enter the third number:")
 
-if st.button("Find Largest Number"):
-    result = largest_number(num1, num2, num3)
-    st.success(f"The largest number is: {result}")
-
+if st.button("Find the Largest Number"):
+    largest = max(num1, num2, num3)
+    st.success(f"The largest number among {num1}, {num2}, and {num3} is: 
+{largest}")
 
